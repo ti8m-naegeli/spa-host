b99ef0b0722a5aa53b5ce8407ca13057827be1c9
Nägeli Andreas <gel@ti8m.ch>
2024-04-16 17:10:15 +0200

    Initial commit

---
+++ SpaHost/Program.cs
@@ -0,0 +1,17 @@
+﻿using GenHTTP.Engine;
+using GenHTTP.Modules.IO;
+using GenHTTP.Modules.Practices;
+using GenHTTP.Modules.SinglePageApplications;
+
+var tree = ResourceTree.FromDirectory("/spa");
+
+var spa = SinglePageApplication.From(tree);
+
+return Host.Create()
+           .Handler(spa)
+           .Defaults()
+           .Console()
+#if DEBUG
+           .Development()
+#endif
+           .Run();
